08c04434c474ce069a0cc837e9cfe034df22896a
Lammi Pekka <pekka.lammi@aalto.fi>
2020-01-15 10:14:24 +0200

    added todolist component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import "./App.css";
 import {InputRow} from './InputRow.js'
+import {TodoList} from './TodoList.js'
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-
-        <h1>Todo app</h1>
+        <h1>My own todos</h1>
 		<InputRow />
+		<TodoList />
       </header>
     </div>
   );
