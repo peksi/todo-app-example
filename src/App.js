345cff525991d48ba1eb6245ca0ab13b6b796b3f
Lammi Pekka <pekka.lammi@aalto.fi>
2020-01-15 10:03:35 +0200

    created input field and button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./App.css";
+import {InputRow} from './InputRow.js'
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Let's make a todo-app here :)</p>
+
+        <h1>Todo app</h1>
+		<InputRow />
       </header>
     </div>
   );
---
+++ src/InputRow.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const InputRow = ()=> {
+	return (
+		<div className="inputContainer">
+			<input type="text" />
+			<button>Submit</button>
+		</div>
+	)
+}
